c3d95eba0cb1c0c0a64791c9ab04fe67feac8794
axmdl1 <axmetali_azia52@mail.ru>
2024-05-23 22:22:14 +0500

    packages, not finished WeightedGraph

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,14 +1,31 @@
 import java.util.*;
 
 public class WeightedGraph<Vertex> {
-    private final boolean undirected;
-    private Map<Vertex, List<Edge<Vertex>>> map = new HashMap<>();
+    private Map<Vertex, List<Edge<Vertex>>> map;
 
     public WeightedGraph() {
-        this(true);
+        map = new HashMap<>();
     }
 
-    public WeightedGraph(boolean undirected) {
-        this.undirected = undirected;
+    public void addVertex(Vertex vertex) {
+        if (!map.containsKey(vertex))
+            map.put(vertex, new ArrayList<>());
+        else
+            throw new IllegalArgumentException("Vertex already exists");
+    }
+
+    public void addEdge(Vertex source, Vertex dest, Double weight) {
+        checkVertex(source);
+        checkVertex(dest);
+        map.get(source).add(new Edge<>(source, dest, weight));
+    }
+
+    private void checkVertex(Vertex vertex) {
+        if (!map.containsKey(vertex))
+            throw new IndexOutOfBoundsException("Vertex already exists");
+    }
+
+    public int countVertices() {
+        return map.size();
     }
 }
--- src/search/DijkstraSearch.java
+++ src/search/DijkstraSearch.java
@@ -1,2 +1,4 @@
+package search;
+
 public class DijkstraSearch {
 }
